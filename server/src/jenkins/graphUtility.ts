46abf14b06266fbd78ad40eaaee51da30fa55327
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2023-11-14 03:09:58 +0100

    .

--- server/src/jenkins/graphUtility.ts
+++ server/src/jenkins/graphUtility.ts
@@ -1,8 +1,5 @@
 import Graph, { DirectedGraph } from "graphology";
 
-export const neighborSetOf = (graph: Graph, node: string): string[] => graph.neighbors(node)
-export const inDegreeOf = (graph: Graph, node: string): number => graph.outDegree(node)
-
 // https://stackoverflow.com/questions/67337917/group-tasks-for-concurrent-processing-in-directed-acyclic-dependency-graph-using
 export const topologicalGenerations = (graph: Graph): string[][] => {
     const groups: string[][] = []
@@ -10,7 +7,7 @@ export const topologicalGenerations = (graph: Graph): string[][] => {
     let group: string[] = []
 
     graph.outboundEdges()
-    Array.from(graph.nodes()).filter(node => inDegreeOf(graph, node) == 0).forEach(node => group.push(node))
+    Array.from(graph.nodes()).filter(node => graph.outDegree(node) == 0).forEach(node => group.push(node))
     // Next we construct all remaining groups. The group k+1 consists of al vertices without incoming arcs if we were
     // to remove all vertices in the previous group k from the graph.
     let result = true
@@ -18,8 +15,8 @@ export const topologicalGenerations = (graph: Graph): string[][] => {
         groups.push([...group]);
         const nextGroup: string[] = []
         for (const task of group) {
-            for (const nextTask of neighborSetOf(graph, task)) {
-                if (inDegreeOf(graph, nextTask) == 1) nextGroup.push(nextTask);
+            for (const nextTask of graph.neighbors(task)) {
+                if (graph.outDegree(nextTask) == 1) nextGroup.push(nextTask);
             }
             graph.dropNode(task); //Removes a vertex and all its edges from the graph
         }
@@ -39,7 +36,7 @@ export const buildDependencyTree = (jobs: { [key: string]: { needs?: string[] } 
         .filter(entry => !!entry.value.needs)
         .flatMap(entry =>
             (entry.value.needs ?? [])
-              .map(needs => ([entry.key, needs] as [string, string]))
+                .map(needs => ([entry.key, needs] as [string, string]))
         )
         .forEach(edge => graph.mergeDirectedEdge(...edge));
 
