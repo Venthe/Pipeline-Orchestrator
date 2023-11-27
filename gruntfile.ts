3eb571248ef683d1d5449131123790d1468c5a6d
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2023-11-27 13:43:17 +0100

    .

--- gruntfile.ts
+++ gruntfile.ts
@@ -6,7 +6,41 @@ import type { PackageJson } from 'pkg-types'
 import { glob } from 'glob'
 import { buildDependencyTree } from './applications/server/src/jenkins/graphUtility'
 import { SpawnOptionsWithoutStdio, spawn } from 'child_process'
+import chalk from 'chalk';
 
+class ColorManager {
+    colors = [chalk.red, chalk.yellow, chalk.green, chalk.blueBright]
+    currentColor = 0
+
+    nextColor() {
+        const ccolor = (this.currentColor + 0)
+        this.currentColor++
+
+        if (this.colors.length < this.currentColor) {
+            this.currentColor = 0
+        }
+
+        return this.colors[ccolor]
+    }
+}
+
+interface Target {
+    async?: boolean
+    pre?: Targets
+}
+
+type Targets = {[key: string]: Target}
+
+const targets: Targets = {
+    "build:watch": {
+        async: true,
+        pre: {
+            "build": {
+                async: false
+            }
+        }
+    }
+}
 
 class Context {
     packages: { [key: string]: PackageJson } = {}
@@ -66,8 +100,9 @@ class Context {
             .map(entry => ({ directory: entry.directory.trim(), dependencies: this.getLocalDependencies(entry.package), name: entry.package.name?.trim() }))
     }
 
-    async execute(task: string) {
+    async execute(target: string) {
         if (!this.tree) throw new Error("Tree not defined")
+        const colorManager = new ColorManager()
 
         for (const i in this.tree) {
             const row = this.tree[i]
@@ -75,26 +110,46 @@ class Context {
             console.group()
             // parallel
             for (const col of row) {
+                const taskConfig: undefined | Target = targets[target]
                 const project = this.workspaceProjects?.filter(wp => wp.name === col)[0]
-                if(!project) throw new Error("This should not happen")
-                console.log(`${project.directory} - ${col}:${task}`)
-                await this.execTask(task, { cwd: project?.directory ?? "" })
+                if (!project) throw new Error("This should not happen")
+
+                const opts = { cwd: project?.directory ?? "", prefix: `${project.directory} - ${col}:${target}`, colorizer: colorManager.nextColor() }
+
+                const runConditionally = async (action: (() => Promise<unknown>), async?: boolean) => {
+                    if (async) {
+                        action()
+                    } else {
+                        await action()
+                    }
+                }
+
+                await Promise.all(Object.entries(taskConfig.pre ?? {}).map(el => runConditionally(async () => this.execTask(el[0], {...opts, prefix: `${project.directory} - ${col}:${el[0]}`}), el[1].async)))
+
+                await runConditionally(async () => this.execTask(target, opts), taskConfig.async)
             }
 
             console.groupEnd()
         }
     }
 
-    async execTask(task: string, opts?: SpawnOptionsWithoutStdio) {
+    async execTask(task: string, opts?: SpawnOptionsWithoutStdio & { prefix?: string, colorizer?: (data: string) => string }) {
+        console.debug(`Executing target ${task}`, opts?.prefix)
+
         return new Promise((resolve, reject) => {
-            const cmd = spawn(`npm`, ['run', task], opts);
+            const cmd = spawn(`npm run --silent ${task}`, [], { ...(opts ?? {}), shell: true });
+
+            const c = opts?.colorizer ?? ((a: string) => a)
+            // TODO: Remove this set :)
+            //  Too many newlines...
+            const withPrefix = (data: string) => c([...new Set(data.split("\n").map(d => opts?.prefix ? `[${opts?.prefix}] ${d}` : d))].join("\n"))
 
             cmd.stdout.on('data', (data) => {
-                console.log(data.toString());
+                console.log(withPrefix(data.toString()));
             });
 
             cmd.stderr.on('data', (data) => {
-                console.error(data.toString());
+                console.error(withPrefix(data.toString()));
             });
 
             cmd.on('exit', (code) => {
@@ -111,12 +166,25 @@ class Context {
 
 grunt.initConfig({})
 
-grunt.registerTask("default", 'A sample task that logs stuff.', async function (...args: string[]) {
+grunt.registerTask("run-many", 'A sample task that logs stuff.', async function (...args: string[]) {
     this.async();
+
+    const target: string | undefined = grunt.option('target')
+    const options: string[] = (grunt.option('projects') as string)?.split(",") ?? []
+
+    if (!target) throw new Error("!")
+
+    console.log(target, options)
 
     const context = new Context("./package.json")
     await context.init()
 
-    context.generateTree() // "@pipeline/core"
-    await context.execute("build")
+    context.generateTree(...options)
+
+    await context.execute(target).catch(e => console.error(e))
+
+    const taskConfig: undefined | Target = targets[target]
+    if (taskConfig?.async) {
+        await new Promise((resolve, reject) => { });
+    }
 })
