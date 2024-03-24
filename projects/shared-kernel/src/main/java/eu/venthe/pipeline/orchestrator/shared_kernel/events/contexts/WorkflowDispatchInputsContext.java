f3de560cf94c565db55f425fff1c34df8c609d0c
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2024-03-24 23:37:12 +0100

    .

--- projects/shared-kernel/src/main/java/eu/venthe/pipeline/orchestrator/shared_kernel/events/contexts/WorkflowDispatchInputsContext.java
+++ projects/shared-kernel/src/main/java/eu/venthe/pipeline/orchestrator/shared_kernel/events/contexts/WorkflowDispatchInputsContext.java
@@ -1,11 +1,32 @@
 package eu.venthe.pipeline.orchestrator.shared_kernel.events.contexts;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import eu.venthe.pipeline.orchestrator.shared_kernel.events.contexts.utilities.ContextUtilities;
+import eu.venthe.pipeline.orchestrator.shared_kernel.events.contexts.utilities.InputUtilities;
 
-// TODO: Implement context
-// components/inputs.yaml#
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class WorkflowDispatchInputsContext {
-    public static WorkflowDispatchInputsContext create(final JsonNode inputs) {
-        throw new UnsupportedOperationException();
+    private final Map<String, InputUtilities.InputValue<?>> inputs;
+
+
+    public WorkflowDispatchInputsContext(final JsonNode _root) {
+        ObjectNode root = ContextUtilities.validateIsObjectNode(_root);
+
+        inputs = root.properties().stream()
+                .map(e -> Map.entry(
+                        e.getKey(),
+                        InputUtilities.describe(e.getValue())
+                ))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue
+                ));
+    }
+
+    public static WorkflowDispatchInputsContext create(final JsonNode _inputs) {
+        return ContextUtilities.ensure(_inputs, WorkflowDispatchInputsContext::new);
     }
 }
---
+++ projects/shared-kernel/src/main/java/eu/venthe/pipeline/orchestrator/shared_kernel/events/contexts/utilities/InputUtilities.java
@@ -0,0 +1,49 @@
+package eu.venthe.pipeline.orchestrator.shared_kernel.events.contexts.utilities;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.experimental.UtilityClass;
+
+import java.math.BigDecimal;
+
+@UtilityClass
+public class InputUtilities {
+    public static InputValue<?> describe(JsonNode node) {
+        if (node.isTextual()) {
+            return InputValue.ofString(node.textValue());
+        } else if (node.isNumber()) {
+            return InputValue.ofNumber(node.decimalValue());
+        } else if (node.isObject()) {
+            return InputValue.ofBoolean(node.booleanValue());
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Value
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class InputValue<T> {
+        T value;
+        Type type;
+
+        public static InputValue<String> ofString(String value) {
+            return new InputValue<>(value, Type.STRING);
+        }
+
+        public static InputValue<BigDecimal> ofNumber(BigDecimal value) {
+            return new InputValue<>(value, Type.NUMBER);
+        }
+
+        public static InputValue<Boolean> ofBoolean(Boolean value) {
+            return new InputValue<>(value, Type.BOOLEAN);
+        }
+
+        public enum Type {
+            STRING,
+            BOOLEAN,
+            NUMBER
+        }
+    }
+}
