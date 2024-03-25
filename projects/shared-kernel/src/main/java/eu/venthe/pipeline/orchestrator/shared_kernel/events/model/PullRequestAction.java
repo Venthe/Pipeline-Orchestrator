dc90ed63ebe8296e712c518f9ce717b5f0a541e4
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2024-03-25 15:27:53 +0100

    .

--- projects/shared-kernel/src/main/java/eu/venthe/pipeline/orchestrator/shared_kernel/events/model/PullRequestAction.java
+++ projects/shared-kernel/src/main/java/eu/venthe/pipeline/orchestrator/shared_kernel/events/model/PullRequestAction.java
@@ -13,43 +13,24 @@ import static java.util.Arrays.stream;
 public enum PullRequestAction {
     ASSIGNED("assigned"),
     AUTO_MERGE_DISABLED("auto_merge_disabled"),
-
     AUTO_MERGE_ENABLED("auto_merge_enabled"),
-
     CLOSED("closed"),
-
     CONVERTED_TO_DRAFT("converted_to_draft"),
-
     DEMILESTONED("demilestoned"),
-
     DEQUEUED("dequeued"),
-
     EDITED("edited"),
-
     ENQUEUED("enqueued"),
-
     LABELED("labeled"),
-
     LOCKED("locked"),
-
     MILESTONED("milestoned"),
-
     OPENED("opened"),
-
     READY_FOR_REVIEW("ready_for_review"),
-
     REOPENED("reopened"),
-
     REVIEW_REQUEST_REMOVED("review_request_removed"),
-
     REVIEW_REQUESTED("review_requested"),
-
     SYNCHRONIZE("synchronize"),
-
     UNASSIGNED("unassigned"),
-
     UNLABELED("unlabeled"),
-
     UNLOCKED("unlocked");
 
     private final String value;
