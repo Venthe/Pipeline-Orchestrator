faa62b4b9b1ac4fed570959f13ab9eda7c78c825
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2024-06-06 15:57:50 +0200

    .

--- src/main/java/eu/venthe/pipeline/orchestrator/shared_kernel/job_execution/NormalJobExecutionContext.java
+++ src/main/java/eu/venthe/pipeline/orchestrator/shared_kernel/job_execution/NormalJobExecutionContext.java
@@ -16,32 +16,9 @@ import java.util.Optional;
 @EqualsAndHashCode
 @SuperBuilder
 @SuppressWarnings("ALL")
-public class NormalJobExecutionContext implements JobContext_ {
-    private final GithubContext github;
-    private final EnvContext env;
-    private final VarsContext vars;
-    private final JobContext job;
-    private final StepsContext steps;
-    private final RunnerContext runner;
-    private final SecretsContext secrets;
-    private final StrategyContext strategy;
-    private final Optional<MatrixContext> matrix;
-    private final NeedsContext needs;
-    private final Optional<InputsContext> inputs;
+public class NormalJobExecutionContext extends CommonJobContext implements JobContext_ {
 
     public NormalJobExecutionContext(JsonNode _root) {
-        ObjectNode root = ContextUtilities.validateIsObjectNode(_root);
-
-        github = GithubContext.ensure(root.get("github"));
-        env = EnvContext.ensure(root.get("env"));
-        vars = VarsContext.ensure(root.get("vars"));
-        job = JobContext.ensure(root.get("job"));
-        steps = StepsContext.ensure(root.get("steps"));
-        runner = RunnerContext.ensure(root.get("runner"));
-        secrets = SecretsContext.ensure(root.get("secrets"));
-        strategy = StrategyContext.ensure(root.get("strategy"));
-        matrix = MatrixContext.create(root.get("matrix"));
-        needs = NeedsContext.ensure(root.get("needs"));
-        inputs = InputsContext.create(root.get("inputs"));
+        super(_root);
     }
 }
--- src/main/java/eu/venthe/pipeline/orchestrator/shared_kernel/job_execution/ReusableWorkflowJobContext.java
+++ src/main/java/eu/venthe/pipeline/orchestrator/shared_kernel/job_execution/ReusableWorkflowJobContext.java
@@ -9,48 +9,21 @@ import lombok.Getter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
-import java.util.Optional;
-
 @Getter
 @ToString
 @EqualsAndHashCode
 @SuperBuilder
 @SuppressWarnings("ALL")
-public class ReusableWorkflowJobContext implements JobContext_ {
-    private final GithubContext github;
-    private final EnvContext env;
-    private final VarsContext vars;
-    private final JobContext job;
+public class ReusableWorkflowJobContext extends CommonJobContext implements JobContext_ {
     /**
-     * The jobs context is only available in reusable workflows, and can only be used to set outputs for a reusable
-     * workflow. For more information, see "Reusing workflows."
-     * <p>
-     * This is only available in reusable workflows, and can only be used to set outputs for a reusable workflow. This
-     * object contains all the properties listed below.
+     * Outputs for a reusable workflow
      */
     private final JobsContext jobs;
-    private final StepsContext steps;
-    private final RunnerContext runner;
-    private final SecretsContext secrets;
-    private final StrategyContext strategy;
-    private final Optional<MatrixContext> matrix;
-    private final NeedsContext needs;
-    private final Optional<InputsContext> inputs;
 
     public ReusableWorkflowJobContext(JsonNode _root) {
-        ObjectNode root = ContextUtilities.validateIsObjectNode(_root);
+        super(_root);
 
-        github = GithubContext.ensure(root.get("github"));
-        env = EnvContext.ensure(root.get("env"));
-        vars = VarsContext.ensure(root.get("vars"));
-        job = JobContext.ensure(root.get("job"));
+        ObjectNode root = ContextUtilities.validateIsObjectNode(_root);
         jobs = JobsContext.ensure(root.get("jobs"));
-        steps = StepsContext.ensure(root.get("steps"));
-        runner = RunnerContext.ensure(root.get("runner"));
-        secrets = SecretsContext.ensure(root.get("secrets"));
-        strategy = StrategyContext.ensure(root.get("strategy"));
-        matrix = MatrixContext.create(root.get("matrix"));
-        needs = NeedsContext.ensure(root.get("needs"));
-        inputs = InputsContext.create(root.get("inputs"));
     }
 }
