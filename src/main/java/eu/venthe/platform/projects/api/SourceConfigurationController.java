bc8b5f3565a09e6e01d9515d9d49dc374b493afb
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2024-11-20 02:08:25 +0100

    Add register endpoint to SourceConfigurationController

---
+++ src/main/java/eu/venthe/platform/projects/api/SourceConfigurationController.java
@@ -0,0 +1,32 @@
+package eu.venthe.platform.projects.api;
+
+import eu.venthe.platform.projects.application.SourceConfigurationCommandService;
+import eu.venthe.platform.projects.application.SourceConfigurationDto;
+import eu.venthe.platform.projects.application.SourceConfigurationQueryService;
+import eu.venthe.platform.shared_kernel.dynamic_value.DynamicValue;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/source-configuration")
+@RequiredArgsConstructor
+public class SourceConfigurationController {
+    private final SourceConfigurationCommandService sourceConfigurationCommandService;
+    private final SourceConfigurationQueryService sourceConfigurationQueryService;
+
+    @PostMapping
+    public String register(@RequestBody RegisterSpecification specification) {
+        return sourceConfigurationCommandService.register(specification.name(), specification.sourceType(), specification.properties());
+    }
+
+    @GetMapping("/{name}")
+    public Optional<SourceConfigurationDto> getSourceConfiguration(@PathVariable String name) {
+        return sourceConfigurationQueryService.getSourceInformation(name);
+    }
+
+    public record RegisterSpecification(String name, String sourceType, Map<String, DynamicValue> properties) {
+    }
+}
